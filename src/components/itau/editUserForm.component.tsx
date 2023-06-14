80ba97770b773bc7165360a67081cdf5e43482f6
boni <felipeboni@4x4brasil.com.br>
2023-06-14 16:08:23 -0300

    fix: fixed itau changed wrong id

--- src/components/itau/editUserForm.component.tsx
+++ src/components/itau/editUserForm.component.tsx
@@ -121,7 +121,7 @@ const EditUserForm = ({ state }: ModalStateProps) => {
         setFetching(true);
 
         const { data } = await FetchWithToken({
-            path: `itau/${modal.data.user_id}`,
+            path: `itau/${modal.data.id}`,
             method: "PUT",
             data: {
                 balance: ReaisToCents(info.balance)
