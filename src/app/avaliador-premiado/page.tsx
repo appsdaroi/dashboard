9cd94a7da51ac241950487d927f14c8ab139f40a
Felipe Boni <53157965+felipeboni@users.noreply.github.com>
2023-06-28 06:44:25 -0300

    feat: added avaliador premiado

---
+++ src/app/avaliador-premiado/page.tsx
@@ -0,0 +1,9 @@
+import { UsersTable } from "../../components/avaliadorPremiado/table.component"
+
+export default async function Profile() {
+    return (
+      <section>
+        <UsersTable />
+      </section>
+    );
+}
